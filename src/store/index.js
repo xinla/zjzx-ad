9de15d3b8dc00275aa68a57bf9aa521947c5cf1e
xinla <1570728529@qq.com>
2019-03-16 18:56:08 +0800

    权限与错误日志

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import app from './modules/app'
-import user from './modules/user'
+import errorLog from './modules/errorLog'
+import permission from './modules/permission'
 import tagsView from './modules/tagsView'
+import user from './modules/user'
 import getters from './getters'
 
 Vue.use(Vuex)
@@ -10,6 +12,8 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     app,
+    errorLog,
+    permission,
     tagsView,
     user
   },
